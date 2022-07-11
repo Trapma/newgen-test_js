06b012cfce2db1875958f1b2aaacb09db05b4c32
Mikhail Trapeznikov <trapma@yandex.ru>
2022-07-12 07:43:59 +1000

    fix

--- utils/getFilterCourse.ts
+++ utils/getFilterCourse.ts
@@ -3,24 +3,29 @@ import { courses } from "./types/types";
 export function getFilterCourse(courses: courses, reqRange: any[]) {
   //    ? нормализуем данные (null to 0)
   const normalizeReqRange = normalize(reqRange);
-  const reqPrice = {
-    start: normalizeReqRange[0],
-    end: normalizeReqRange[1],
-  };
 
   return courses.filter((course) => {
     //   ? нормализуем массив
     const prices = normalize(course.prices);
+    const reqPrice = {
+      start: normalizeReqRange[0],
+      end: !normalizeReqRange[1] ? prices[1] : normalizeReqRange[1],
+    };
     const coursePrice = {
       start: prices[0],
-      end: prices[1],
+      end: !prices[1] && reqPrice.end ? reqPrice.end : prices[1],
     };
 
     let condition: boolean = false;
 
-    const start = Math.max(coursePrice.start, reqPrice.start);
-    const end = Math.min(coursePrice.end, reqPrice.end);
-    if (start < end || start == end) {
+    if (!reqPrice.end && !coursePrice.end) {
+      condition = true;
+    } else {
+      const start = Math.max(coursePrice.start, reqPrice.start);
+      const end = Math.min(coursePrice.end, reqPrice.end);
+      condition = start < end || start == end;
+    }
+    if (condition) {
       return {
         name: course.name,
         prices,
