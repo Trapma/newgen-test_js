3686ffbe2bd45204d3456137aad9ce59de02f7b8
Mikhail Trapeznikov <trapma@yandex.ru>
2022-07-12 06:44:23 +1000

    refactor filterCourse

--- utils/getFilterCourse.ts
+++ utils/getFilterCourse.ts
@@ -18,35 +18,9 @@ export function getFilterCourse(courses: courses, reqRange: any[]) {
 
     let condition: boolean = false;
 
-    // от 100 до Х
-    if (!reqPrice.end) {
-      if (!coursePrice.end) {
-        condition = true;
-      } else {
-        condition = coursePrice.end > reqPrice.start;
-      }
-    }
-    // от 0 до 100
-    if (!reqPrice.start) {
-      if (!coursePrice.start) {
-        condition = true;
-      }
-      condition = coursePrice.start < reqPrice.end;
-    }
-    // от 100 до 200
-    if (reqPrice.start && reqPrice.end) {
-      if (!coursePrice.start && !coursePrice.end) {
-        return true;
-      } else if (!coursePrice.start) {
-        condition = coursePrice.end > reqPrice.start;
-      } else if (!coursePrice.end) {
-        condition = coursePrice.start < reqPrice.end;
-      } else {
-        condition = coursePrice.start < reqPrice.end || coursePrice.end > reqPrice.start;
-      }
-    }
-
-    if (condition) {
+    const start = Math.max(coursePrice.start, reqPrice.start);
+    const end = Math.max(coursePrice.end, reqPrice.end);
+    if (start < end || start == end) {
       return {
         name: course.name,
         prices,
